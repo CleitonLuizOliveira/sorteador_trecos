06eb6735432f59b5fe2cda1d96d773eda468c206
CleitonLuizOliveira <cleitoneldorn@gmail.com>
2020-10-31 15:07:34 -0300

    Criei o app, sme estilo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [lastID, setLastID] = useState(1);
+  const [sortData, setSortData] = useState([{id:0, value: ''},{id:1, value: ''}]);
+  const [sortedResult, setSortedResult] = useState('');
+
+  function ChangeSortData(index, key, value) {
+    const tempArray = sortData.map(item => 
+        item.id === index ? {...item, [key]: value } : item
+    );
+    setSortData(tempArray);
+  }
+
+  function AddItem() {
+    const newLine = {id: lastID + 1, value: ''};
+    const tempArray = sortData.map(item => item);
+    tempArray.push(newLine);
+    setSortData(tempArray);
+    setLastID(lastID + 1);
+  }
+
+  function RemoveItem(index) {
+    const tempArray = sortData.filter(item => item.id !== index);
+    setSortData(tempArray);
+}
+
+  function SortResult() {
+    const result = sortData[Math.floor(Math.random() * sortData.length)].value;
+    setSortedResult(result);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {sortData.map(item => (
+        <div className="input_group" key={item.id}>
+          <input type="text" value={item.value} onChange={event => (ChangeSortData(item.id,"value", event.currentTarget.value))} />          
+          <button onClick={() => RemoveItem(item.id)} disabled={item.id < 2} >-</button>
+          <button onClick={AddItem} >+</button>
+        </div> 
+      ))}
+      <button onClick={SortResult} >Sortear</button>
+
+      <p> {sortedResult} </p>
+
+
     </div>
   );
 }
